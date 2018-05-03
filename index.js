5c991d02d91a3bc5f5bbec8db2b339ddfe8759b2
Carles Capellas <carles.capellas@adp.com>
2018-05-03 17:45:26 +0200

    Replace countries code with its population. Limit countries endpoint results to a maxium of ten. Support filtering to refine searches. Add a delay to simulate bad network conditions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const countries = require('./countries')
+const countries = require('./countries').sort((a, b) => b.population - a.population)
 
 app.use(express.static('public'))
 
@@ -10,8 +10,15 @@ const corsMiddleware = (req, res, next) => {
 	res.header("Access-Control-Allow-Credentials", "true");
 	next();
 };
+
 app.use('/api/countries', corsMiddleware, (req, res, next) => {
-	return res.json(countries)
+	var filter = req.query.filter;
+	var filteredCountries = countries;
+	if (filter && filter != '') {
+		filteredCountries = countries.filter(c =>
+			c.name.toLowerCase().indexOf(filter.toLowerCase()) > -1);
+	}
+	setTimeout(() => res.json(filteredCountries.slice(0, 10)), 5000);
 });
 
 app.listen(3000, () => console.log('App running on port 3000'))
