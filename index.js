6fd46a30ede62d542c8687767064255b775b7eb9
Carles Capellas <carles.capellas@adp.com>
2018-05-03 10:29:07 +0200

    Modify express app to serve public/index.html file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Sample node.js server'))
+app.use(express.static('public'))
 
 app.listen(3000, () => console.log('App running on port 3000'))
