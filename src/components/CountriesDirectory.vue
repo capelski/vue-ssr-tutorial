9818153f0b5bb46d16757dac016be139c75e6b7e
Carles Capellas <carles.capellas@adp.com>
2018-05-03 18:06:41 +0200

    Add input field to filter countries by name

--- src/components/CountriesDirectory.vue
+++ src/components/CountriesDirectory.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
+    <h2>Countries directory</h2>
+    <input v-model="filter" v-on:change="loadCountries" class="form-control" type="text" placeholder="Country name" />
     <loader v-if="loading"/>
     <ul v-if="!loading">
       <li v-for="(country, index) in countries" v-bind:key="index">
-        {{ country.name }}
+        <b>{{ country.name }}</b> {{ country.population }}
       </li>
       <div v-if="!countries.length"><i>No countries available</i></div>
     </ul>
@@ -20,15 +22,22 @@ export default {
   data () {
     return {
       countries: [],
+      filter: '',
       loading: true
     }
   },
   created() {
-    get('countries', null, [])
-    .then(countries => {
-      this.countries = countries
-      this.loading = false
-    })
+    this.loadCountries()
+  },
+  methods: {
+    loadCountries() {
+      this.loading = true;
+      get('countries', { filter: this.filter }, [])
+      .then(countries => {
+        this.countries = countries
+        this.loading = false
+      })
+    }
   }
 }
 </script>
@@ -40,5 +49,18 @@ export default {
   li {
     list-style-type: none;
     padding: 5px;
+  }
+  .form-control {
+    padding: 6px 12px;
+    padding: .375rem .75rem;
+    font-size: 16px;
+    font-size: 1rem;
+    line-height: 1.5;
+    color: #495057;
+    background-color: #fff;
+    background-clip: padding-box;
+    border: 1px solid #ced4da;
+    border-radius: .25rem;
+    transition: border-color .15s 
   }
 </style>
