0b02bec5991bb0969121d3ed5c19e8ee23c5a250
Carles Capellas <carles.capellas@adp.com>
2018-05-03 11:54:24 +0200

    Add countries list. Add endpoint to retrieve countries. Allow CORS requests on countries endpoints

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
+const countries = require('./countries')
 
 app.use(express.static('public'))
+
+const corsMiddleware = (req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Credentials", "true");
+	next();
+};
+app.use('/api/countries', corsMiddleware, (req, res, next) => {
+	return res.json(countries)
+});
 
 app.listen(3000, () => console.log('App running on port 3000'))
