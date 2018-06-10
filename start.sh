462ab7b7eeeba0a21afd374aca4cc6cf0b123162
Carles Capellas Careta <capellas.carles@gmail.com>
2018-06-10 12:33:42 +0200

    Create bash script to start up the project

---
+++ start.sh
@@ -0,0 +1,12 @@
+cd ui
+echo "Installing vue app npm packages..."
+npm install
+echo "Generating vue app static files..."
+npm run build
+echo "Copying vue app static files to server assets folder..."
+cp -rf dist/** ../server/public
+cd ../server
+echo "Installing server npm packages..."
+npm install
+echo "Starting nodejs server..."
+npm start &
