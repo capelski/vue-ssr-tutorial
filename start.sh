be5b20510367c3a75e3fdc3872dd4c587903b738
Carles Capellas Careta <capellas.carles@gmail.com>
2018-06-10 12:49:15 +0200

    Modify start up script to copy prerendered files too

--- start.sh
+++ start.sh
@@ -5,6 +5,8 @@ echo "Generating vue app static files..."
 npm run build
 echo "Copying vue app static files to server assets folder..."
 cp -rf dist/** ../server/public
+echo "Copying prerendered vue app files to server assets folder..."
+cp -rf prerendered/** ../server/public
 cd ../server
 echo "Installing server npm packages..."
 npm install
