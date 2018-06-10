c37a23454c2c39d61cacfe0118ca0997a3cd8d8c
Carles Capellas Careta <capellas.carles@gmail.com>
2018-06-10 13:47:52 +0200

    Modify start up script to make server available before prerendering

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
-cd ui
+cd server
+echo "Installing server npm packages..."
+npm install
+echo "Launching server to make web api available for prerendering..."
+npm start &
+server_pid=$!
+cd ../ui
 echo "Installing vue app npm packages..."
 npm install
 echo "Generating vue app static files..."
@@ -7,8 +13,4 @@ echo "Copying vue app static files to server assets folder..."
 cp -rf dist/** ../server/public
 echo "Copying prerendered vue app files to server assets folder..."
 cp -rf prerendered/** ../server/public
-cd ../server
-echo "Installing server npm packages..."
-npm install
-echo "Starting nodejs server..."
-npm start
+echo "Server ready..."
