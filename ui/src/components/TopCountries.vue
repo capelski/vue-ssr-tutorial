9c1f7463f1f0f806220796040789451ea139804b
Carles Capellas Careta <capellas.carles@gmail.com>
2018-09-29 16:08:28 +0200

    Remove TopCountries created hook. Make client-entry wait for asyncData

--- ui/src/components/TopCountries.vue
+++ ui/src/components/TopCountries.vue
@@ -10,28 +10,30 @@ import { get } from '../http.js';
 import Loader from './Loader';
 import CountryList from './CountryList';
 
-function getCountries() {
-  return get('countries', null, [])
-    .then(countries => {
-      this.countries = countries
-      this.loading = false
-    });
-}
+let _countries = [];
+let _loading = true;
 
 export default {
   name: 'TopCountries',
   components: { Loader, CountryList },
   data () {
     return {
-      countries: [],
-      loading: true
+      countries: _countries,
+      loading: _loading
     }
   },
   asyncData() {
-    return getCountries.apply(this);
-  },
-  created() {
-    getCountries.apply(this);
+    // Be aware that the 'this' in the asyncData method is not the same object
+    // than the actual object that Vue ends app rendering in the DOM.
+    // If we modfiy the this.countries on the server response, the list of
+    // countries will not get updated!
+    return get('countries', null, [])
+      .then(countries => {
+        _countries = countries
+        _loading = false
+        // The countries are being returned for manually assigning them the first time the page is loaded
+        return countries;
+      });
   }
 }
 </script>
